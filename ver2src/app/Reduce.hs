b3f6397fd93c201469e2233572ac7b8a7d5c51f8
righ1113 <34955220+righ1113@users.noreply.github.com>
2019-06-13 11:06:45 +0900

    19/06/13　Reduce.hs : stripSub2の実装

--- ver2src/app/Reduce.hs
+++ ver2src/app/Reduce.hs
@@ -9,16 +9,16 @@ module Main where
 -- module Reduce where
 
 {-
-import Control.Arrow ((<<<))
 import Debug.Trace (trace)
 -}
 import Data.List (find)
 import Data.Maybe (fromJust, isJust)
+import Control.Arrow ((<<<))
 import Control.Lens ((&), (.~), ix, (^?!), _1, _2, _4, _5, _6, _7, (^.))
 import Lib (TpConfmat, readFileUnavoidR, myLoop)
 
 
-verts   = 27 -- max number of vertices in a free completion + 1
+mVerts  = 27 -- max number of vertices in a free completion + 1
 deg     = 13 -- max degree of a vertex in a free completion + 1
 -- must be at least 13 because of row 0
 edges   = 62 -- max number of edges in a free completion + 1
@@ -26,6 +26,7 @@ maxring = 14 -- max ring-size
 
 type TpAngle  = [[Int]]
 type TpEdgeno = [[Int]]
+type TpF4 = (Bool, Int, TpEdgeno) -> Int -> ((Bool, Int, TpEdgeno) -> (Bool, Int, TpEdgeno)) -> (Bool, Int, TpEdgeno)
 
 
 main :: IO ()
@@ -74,9 +75,119 @@ findangles graph =
 strip :: TpConfmat -> TpEdgeno
 strip graph =
   let
-    verts = (graph ^?! ix 0) ^?! ix 0
-    ring  = (graph ^?! ix 0) ^?! ix 1
+    verts                   = (graph ^?! ix 0) ^?! ix 0
+    ring                    = (graph ^?! ix 0) ^?! ix 1
+    edgeno0                 = replicate edges $ replicate edges 0
+    edgeno1                 = (edgeno0 & (ix ring <<< ix 1) .~ 1) & (ix 1 <<< ix ring) .~ 1
+    f1 n x cont             = cont $ stripSub1 n x
+    edgeno2                 = myLoop f1 id edgeno1 [2 .. ring]
+    done0                   = replicate mVerts False
+    term0                   = 3 * (verts - 1) - ring
+    f2 n x cont             = cont $ stripSub2 graph verts ring n x
+    (edgeno3, done1, term1) = myLoop f2 id (edgeno2, done0, term0) [(ring + 1) .. verts]
+    f3 n x cont             = cont $ stripSub3 graph verts ring n x
+    (edgeno4, done2, term2) = myLoop f3 id (edgeno3, done1, term1) [1 .. ring]
   in [[]]
+
+stripSub3Sub1 :: TpConfmat -> [Bool] -> Int -> (Int, Int) -> Int -> (Int, Int)
+stripSub3Sub1 graph done ring (maxint, best) v =
+  if done !! v
+    then (maxint, best)
+    else
+      let
+        u     = if v > 1    then v - 1 else ring
+        w     = if v < ring then v + 1 else 1
+        inter = 3 * (graph ^?! ix v ^?! ix 1) + 4 * (fromEnum (done !! u) + fromEnum (done !! w))
+      in  if inter > maxint
+            then (inter,  v   )
+            else (maxint, best)
+
+stripSub3 :: TpConfmat -> Int -> Int -> (TpEdgeno, [Bool], Int) -> Int -> (TpEdgeno, [Bool], Int)
+stripSub3 graph verts ring (edgeno0, done0, term0) x =
+  let
+    f1 n x cont           = cont $ stripSub3Sub1 graph done0 ring n x
+    (maxint, best)        = myLoop f1 id (0, 0) [1 .. ring]
+{-    f2 n x cont           = cont $ stripSub2Sub2 graph max1 n x
+    (maxdeg, hoge)        = myLoop f2 id (0, 0) [1 .. maxes]
+    grav                  = graph !! best
+    d                     = grav !! 1
+    f3 n@(retB, _, _, p2) x cont
+      | not p2    = n
+      | retB      = n
+      | otherwise = cont $ stripSub2Sub3 grav done0 d n x
+    (_, first, _, _)      = myLoop f3 id (False, 1, done0 !! (grav !! (d + 1)), True) [0 .. 64]
+    f4 n@(retB, _, _) x cont
+      | retB      = n
+      | otherwise = cont $ stripSub2Sub4 grav done0 d best first f4 n x
+    (_, term1, edgeno1)   = myLoop f4 id (False, term0, edgeno0) [first .. 64] -}
+  in (edgeno0, done0 & ix best .~ True, term0)
+
+stripSub1 :: TpEdgeno -> Int -> TpEdgeno
+stripSub1 edgeno v = (edgeno & (ix (v - 1) <<< ix v) .~ v) & (ix v <<< ix (v - 1)) .~ v
+
+-- First we find all vertices from the interior that meet the "done"
+-- vertices in an interval, and write them in max[1] .. max[maxes]
+stripSub2 :: TpConfmat -> Int -> Int -> (TpEdgeno, [Bool], Int) -> Int -> (TpEdgeno, [Bool], Int)
+stripSub2 graph verts ring (edgeno0, done0, term0) x =
+  let
+    max0                  = replicate mVerts 0
+    f1 n x cont           = cont $ stripSub2Sub1 graph done0 n x
+    (maxint, maxes, max1) = myLoop f1 id (0, 0, max0) [(ring + 1) .. verts]
+    f2 n x cont           = cont $ stripSub2Sub2 graph max1 n x
+    (maxdeg, best)        = myLoop f2 id (0, 0) [1 .. maxes]
+    grav                  = graph !! best
+    d                     = grav !! 1
+    f3 n@(retB, _, _, p2) x cont
+      | not p2    = n
+      | retB      = n
+      | otherwise = cont $ stripSub2Sub3 grav done0 d n x
+    (_, first, _, _)      = myLoop f3 id (False, 1, done0 !! (grav !! (d + 1)), True) [0 .. 64]
+    f4 n@(retB, _, _) x cont
+      | retB      = n
+      | otherwise = cont $ stripSub2Sub4 grav done0 d best first f4 n x
+    (_, term1, edgeno1)   = myLoop f4 id (False, term0, edgeno0) [first .. 64]
+  in (edgeno1, done0 & ix best .~ True, term1)
+
+stripSub2Sub1 :: TpConfmat -> [Bool] -> (Int, Int, [Int]) -> Int -> (Int, Int, [Int])
+stripSub2Sub1 graph done (maxint, maxes, max) v =
+  if done !! v
+    then (maxint, maxes, max)
+    else
+      let inter = inInterval (graph !! v) done
+      in  if inter > maxint
+            then   (inter,  1,         max & ix 1 .~ v          )
+            else if inter == maxint
+              then (maxint, maxes + 1, max & ix (maxes + 1) .~ v)
+              else (maxint, maxes,     max                      )
+
+-- From the terms in max we choose the one of maximum degree
+stripSub2Sub2 :: TpConfmat -> [Int] -> (Int, Int) -> Int -> (Int, Int)
+stripSub2Sub2 graph max (maxdeg, best) h =
+  let
+    d = (graph ^?! ix (h + 1)) ^?! ix 0
+  in if d > maxdeg then (d, max !! h) else (maxdeg, best)
+
+-- So now, the vertex "best" will be the next vertex to be done
+stripSub2Sub3 :: [Int] -> [Bool] -> Int -> (Bool, Int, Bool, Bool) -> Int -> (Bool, Int, Bool, Bool)
+stripSub2Sub3 grav done d (_, first0, _, _) _ =
+  let first1   = first0 + 1
+      previous = done !! (grav !! (first1 + 1))
+  in  if first0 > d then (True, 1, previous, True)
+      else if not previous && done !! (grav !! (first0 + 1)) then (False, first1, previous, False) else (False, first1, previous, True)
+
+-- This eventually lists all the internal edges of the configuration
+stripSub2Sub4 :: [Int] -> [Bool] -> Int -> Int -> Int -> TpF4 -> (Bool, Int, TpEdgeno) -> Int -> (Bool, Int, TpEdgeno)
+stripSub2Sub4 grav done d best first f4 (_, term0, edgeno0) h =
+  if not $ done !! (grav !! (h + 1))
+    then (True, term0, edgeno0)
+    else
+      let edgeno1 = (edgeno0 & (ix best <<< ix (grav !! (h + 1))) .~ term0) & (ix (grav !! (h + 1)) <<< ix best) .~ term0
+          term1   = term0 - 1
+      in  if h == d
+            then if first == 1
+              then (True, term1, edgeno1)
+              else myLoop f4 id (False, term1, edgeno1) [0 .. 64]
+            else (False, term1, edgeno1)
 
 -- if grav meets the done vertices in an interval of length >=1, it returns
 -- the length of the interval, and otherwise returns 0
@@ -115,5 +226,6 @@ inIntervalSub grav done (_, l, w) j
   | done !! (grav !! j) = (1, l + 1, True)
   | w                   = (0, l,     w   )
   | otherwise           = (1, l,     w   )
+
 
 
