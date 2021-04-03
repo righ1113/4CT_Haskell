f6ce7e506242d71049fe7a4d7e2811c6fcc62422
righ1113 <pykp42557@mineo.jp>
2021-04-04 00:05:40 +0900

    21/04/04 Reduce : findangles() の実装3

--- ver4src/src/ReLibAngles.hs
+++ ver4src/src/ReLibAngles.hs
@@ -6,21 +6,15 @@ import Control.Lens  ( (&), (.~), Ixed(ix) )
 
 
 findangles :: TpConfmat -> TpEdgeno -> (TpAngle, TpAngle, TpAngle, [Int])
-findangles gConf edgeno = (angle3, diffangle3, sameangle0, contract0) where
+findangles gConf edgeno = (angle3, diffangle3, sameangle0, contract3) where
   contract0  = replicate (edges + 1) 0
   angle0     = replicate edges $ replicate 5 0
   diffangle0 = replicate edges $ replicate 5 0
   sameangle0 = replicate edges $ replicate 5 0
 
-  {- @contract[0] = g_conf[1 + 1][0] -- number of edges in contract
-  @contract[Const::EDGES] = g_conf[0 + 1][3]
-  @contract[0].times do |ii|
-    i = ii + 1
-    u = g_conf[2][2 * i - 1]
-    v = g_conf[2][2 * i]
-    Assert.assert_equal (edgeno[u][v] >= 1), true, '***  ERROR: CONTRACT CONTAINS NON-EDGE  ***'
-    @contract[edgeno[u][v]] = 1
-  end -}
+  contract1 = contract0  & ix 0     .~ head (gConf !! (1 + 1)) -- number of edges in contract
+  contract2 = contract1  & ix edges .~ (gConf !! (0 + 1)) !! 3
+  contract3 = findanglesSub1 gConf edgeno contract2 1
 
   edge       = 3 * head (gConf !! (0 + 1))  - 3 - (gConf !! (0 + 1)) !! 1
   diffangle1 = diffangle0  & (ix 0 <<< ix 0) .~ head (gConf !! (0 + 1))
@@ -30,6 +24,15 @@ findangles gConf edgeno = (angle3, diffangle3, sameangle0, contract0) where
   angle1 = angle0  & (ix 0 <<< ix 0) .~ head (head diffangle3)
   angle2 = angle1  & (ix 0 <<< ix 1) .~ head diffangle3 !! 1
   angle3 = angle2  & (ix 0 <<< ix 2) .~ head diffangle3 !! 2
+
+findanglesSub1 :: TpConfmat -> TpEdgeno -> [Int] -> Int -> [Int]
+findanglesSub1 gConf edgeno contract i
+  | i > head contract      = contract
+  | (edgeno !! u) !! v < 1 = error "***  ERROR: CONTRACT CONTAINS NON-EDGE  ***"
+  | otherwise              = findanglesSub1 gConf edgeno contract2 (i + 1) where
+    u         = (gConf !! 2) !! (2 * i - 1)
+    v         = (gConf !! 2) !! (2 * i)
+    contract2 = contract  & ix ((edgeno !! u) !! v) .~ 1
 
 
 
