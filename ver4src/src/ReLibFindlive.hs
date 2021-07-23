ac52e4858f3d75490aabd324b500c8fbe593ba12
righ1113 <pykp42557@mineo.jp>
2021-07-23 10:02:52 +0900

    21/07/23 Reduce : findlive() の実装3

--- ver4src/src/ReLibFindlive.hs
+++ ver4src/src/ReLibFindlive.hs
@@ -9,7 +9,7 @@ import Control.Lens  ( (&), (.~), Ixed(ix) )
 -- free extension. Returns the number of such codes
 findlive :: Int -> Int -> [Int] -> Int -> TpAngle -> [Int] -> Int -> IO (Int, [Int])
 findlive ring bigno live ncodes angle _power extentclaim
-  = findliveSub bigno live angle ring ed extentclaim ncodes j c3 forbidden2 where
+  = findliveSub bigno live angle ring ed extentclaim ncodes j c3 forbidden2 0 0 where
       ed             = head angle !! 2
       c              = replicate edges 0
       j              = ed - 1
@@ -19,9 +19,13 @@ findlive ring bigno live ncodes angle _power extentclaim
       forbidden2     = forbidden & ix j .~ 5
 
 
-findliveSub :: Int -> [Int] -> TpAngle -> Int -> Int -> Int -> Int -> Int -> [Int] -> [Int] -> IO (Int, [Int])
-findliveSub _bigno live _angle _ring _ed _extentclaim _ncodes _j _c _forbidden
-  = return (1, live)
+findliveSub :: Int -> [Int] -> TpAngle -> Int -> Int -> Int -> Int -> Int -> [Int] -> [Int] -> Int -> Int -> IO (Int, [Int])
+findliveSub _bigno live _angle _ring _ed _extentclaim _ncodes _j _c _forbidden _extent 262144
+  -- Assert.assert_equal (1 == 2), true, 'findlive_sub : It was not good though it was repeated 262144 times!'
+  = return (-1, live)
+findliveSub bigno live angle ring ed extentclaim ncodes j c forbidden extent cnt
+  = findliveSub bigno live angle ring ed extentclaim ncodes j c forbidden extent (cnt + 1)
+  -- = return (1, live)
 
 
 
