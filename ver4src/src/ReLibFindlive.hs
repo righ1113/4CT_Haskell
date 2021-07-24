7617c8c18f96daaf7ecbb32765160144e9f3b8af
righ1113 <pykp42557@mineo.jp>
2021-07-25 06:21:21 +0900

    21/07/25 Reduce : findlive() の実装4

--- ver4src/src/ReLibFindlive.hs
+++ ver4src/src/ReLibFindlive.hs
@@ -2,6 +2,7 @@ module ReLibFindlive where
 
 import CoLibCConst   ( edges, TpAngle )
 import Control.Lens  ( (&), (.~), Ixed(ix) )
+import Data.Bits     ( Bits((.&.), shift) )
 
 
 -- computes {\cal C}_0 and stores it in live. That is, computes codes of
@@ -21,11 +22,29 @@ findlive ring bigno live ncodes angle _power extentclaim
 
 findliveSub :: Int -> [Int] -> TpAngle -> Int -> Int -> Int -> Int -> Int -> [Int] -> [Int] -> Int -> Int -> IO (Int, [Int])
 findliveSub _bigno live _angle _ring _ed _extentclaim _ncodes _j _c _forbidden _extent 262144
-  -- Assert.assert_equal (1 == 2), true, 'findlive_sub : It was not good though it was repeated 262144 times!'
+  -- = error "findlive_sub : It was not good though it was repeated 262144 times!"
   = return (-1, live)
 findliveSub bigno live angle ring ed extentclaim ncodes j c forbidden extent cnt
-  = findliveSub bigno live angle ring ed extentclaim ncodes j c forbidden extent (cnt + 1)
-  -- = return (1, live)
+  | exit      = return (ncodes - extent, live)
+  | otherwise = findliveSub bigno live angle ring ed extentclaim ncodes j c2 forbidden extent (cnt + 1) where
+      (exit, c2) = (False, c2) --findliveSubSub1 forbidden j c ed ring ncodes extent extentclaim
+
+
+findliveSubSub1 :: [Int] -> Int -> [Int] -> Int -> Int -> Int -> Int -> Int -> (Bool, [Int])
+findliveSubSub1 forbidden j c ed ring ncodes extent extentclaim
+  | (forbidden !! j) .&. (c !! j) == 0 = (False, c)
+  | exit                               = (True, c3)
+  | otherwise                          = findliveSubSub1 forbidden j c3 ed ring ncodes extent extentclaim where
+      c2         = c & ix j .~ shift (c !! j) 1
+      (exit, c3) = findliveSubSub1Sub forbidden j c2 ed ring ncodes extent extentclaim
+
+
+findliveSubSub1Sub :: [Int] -> Int -> [Int] -> Int -> Int -> Int -> Int -> Int -> (Bool, [Int])
+findliveSubSub1Sub forbidden j c ed ring ncodes extent extentclaim
+  | 8 .&. (c !! j) == 0 = (False, c)
+  | j > ed -1           = (True, c) -- print_status ring, ncodes, extent, extentclaim
+  | otherwise           = findliveSubSub1Sub forbidden j c2 ed ring ncodes extent extentclaim where
+      c2 = c & ix j .~ shift (c !! j) 1
 
 
 
