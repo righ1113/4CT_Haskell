230453394f6b8fe0f5e19255a7b2223e908a7209
righ1113 <34955220+righ1113@users.noreply.github.com>
2022-09-18 09:02:12 +0900

    22/09/18 Reduce : checkCReduce() の実装12

--- ver5src/src/ReLibCRedu.hs
+++ ver5src/src/ReLibCRedu.hs
@@ -5,8 +5,11 @@ module ReLibCRedu( checkCReduce ) where
 import CoLibCConst
     ( edges,
       power )
+import Control.Applicative            ( empty )
 import Control.Lens                   ( (&), (.~), Ixed(ix) )
-import Data.Bits                      ( Bits(shift, (.&.), (.|.)) )
+import Control.Monad.IO.Class         ( liftIO )
+import Control.Monad.Trans.Maybe      ( MaybeT(..) )
+import Data.Bits                      ( Bits(shift, (.&.), (.|.)), complement )
 import Data.Function                  ( fix )
 
 
@@ -27,44 +30,74 @@ checkCReduce ring bigno nLive live diffangle sameangle contract = do
     sm         = sameangle !! j2
     c3         = c2 & ix j2 .~ 1
     u          = 4
-    imax1      = dm !! 0 - 1
+    imax1      = dm !! 0
     u2         = foldl (\x y -> x .|. c3 !! y) u  (take imax1 . tail $ dm)
-    imax2      = sm !! 0 - 1
+    imax2      = sm !! 0
     u3         = foldl (\x y -> x .|. c3 !! y) u2 (take imax2 . tail $ sm)
     forbidden2 = forbidden & ix j2 .~ u3
 
-  checkCReduceSub 0 forbidden2 c3 contract j2 start2 diffangle sameangle bigno ring live
+  putStrLn ("f c j: " ++ show forbidden ++ " " ++ show c3 ++ " " ++ show j2)
+  _ <- runMaybeT $ checkCReduceSub 0 forbidden2 c3 contract j2 start2 diffangle sameangle bigno ring live
+  return True
 
 
-checkCReduceSub :: Int -> [Int] -> [Int] -> [Int] -> Int -> Int -> [[Int]] -> [[Int]] -> Int -> Int -> [Int] -> IO Bool
+checkCReduceSub :: Int -> [Int] -> [Int] -> [Int] -> Int -> Int -> [[Int]] -> [[Int]] -> Int -> Int -> [Int] -> MaybeT IO Bool
 checkCReduceSub 2097152 _ _ _ _ _ _ _ _ _ _      = error "checkCReduceSub : It was not good though it was repeated 2097152 times!"
-checkCReduceSub cnt forbidden c contract j start diffangle sameangle bigno ring live
-  | j1 == one && not (inLive c1 ring live bigno) = error "ERROR: INPUT CONTRACT IS INCORRECT  ***\n\n"
-  | j1 == 1 && ret2                              = do {putStrLn "               ***  Contract confirmed ***"; return True}
-  | j1 <= 0                                      = error "checkCReduceSub : error!"
-  | otherwise                                    = checkCReduceSub (cnt + 1) forbidden c2 contract j2 start diffangle sameangle bigno ring live where
-      one = 1::Int
-      (_,    c1, j1) = ccrSubSub1 0 c  j  contract start forbidden
-      (ret2, c2, j2) = ccrSubSub2 0 c1 j1 contract start
+checkCReduceSub cnt forbidden c contract j start diffangle sameangle bigno ring live = do
+  --liftIO $ putStrLn "checkCReduceSub start."
+  (c1, j1) <- ccrSubSub1 0 c  j  contract start forbidden
+  --let (c1, j1) = (c, j)
+  --liftIO $ putStrLn "checkCReduceSub chk 1."
+  _        <- if j1 == 1 && not (inLive c1 ring live bigno) then error "ERROR: INPUT CONTRACT IS INCORRECT  ***\n\n" else return True
+  (c2, j2) <- ccrSubSub2 0 (c1 & ix j1 .~ shift (c1 !! j1) 1) j1 contract start
+  _        <- if j1 == 1 then checkCReduceSub (cnt + 1) forbidden c2 contract j2 start diffangle sameangle bigno ring live else return True
+  _        <- if j2 <= 0 then error "checkCReduceSub : error!" else return True
+  let
+    j3         = ccrSubSub3 j1 contract
+    dm         = diffangle !! j3
+    sm         = sameangle !! j3
+    c3         = c1 & ix j3 .~ 1
+    u1         = 0
+    u2         = ccrSubSub4 u1 c3 dm sm 1
+    forbidden2 = forbidden & ix j3 .~ u2
+  checkCReduceSub (cnt + 1) forbidden2 c3 contract j3 start diffangle sameangle bigno ring live
 
 
-ccrSubSub1 :: Int -> [Int] -> Int -> [Int] -> Int -> [Int] -> (Bool, [Int], Int)
-ccrSubSub1 0 _ _ _ _ _ = error "ccrSubSub1 rec error!!"
+ccrSubSub1 :: Int -> [Int] -> Int -> [Int] -> Int -> [Int] -> MaybeT IO ([Int], Int)
+ccrSubSub1 100000 _ _ _ _ _ = error "ccrSubSub1 rec error!!"
 ccrSubSub1 cnt c j contract start forbidden
-  | forbidden !! j .&. c !! j == 0 = (False, c, j)
-  | ret2                           = (True, c2, j2)
-  | otherwise                      = ccrSubSub1 (cnt + 1) c2 j2 contract start forbidden where
-      (ret2, c2, j2) = (True, c, j) --ccrSubSub2 0 c j contract start
+  | b         = return (c, j)
+  | otherwise = do
+      --liftIO $ putStrLn ("f c j: " ++ show forbidden ++ " " ++ show c ++ " " ++ show j)
+      (c3, j2) <- ccrSubSub2 0 c2 j contract start
+      ccrSubSub1 (cnt + 1) c3 j2 contract start forbidden where
+        b  = True --forbidden !! j .&. c !! j == 0
+        c2 = c & ix j .~ shift (c !! j) 1
 
 
-ccrSubSub2 :: Int -> [Int] -> Int -> [Int] -> Int -> (Bool, [Int], Int)
-ccrSubSub2 1000 _ _ _ _ = error "ccrSubSub2 rec error!!"
+ccrSubSub2 :: Int -> [Int] -> Int -> [Int] -> Int -> MaybeT IO ([Int], Int)
+ccrSubSub2 100000 _ _ _ _ = error "ccrSubSub2 rec error!!"
 ccrSubSub2 cnt c j contract start
-  | c2 !! j .&. 8 == 0 = (False, c2, j)
-  | j2 >= start        = (True, c2, j2)
-  | otherwise          = ccrSubSub2 (cnt + 1) (c2 & ix j2 .~ shift (c2 !! j2) (-1)) j2 contract start where
-      c2 = c & ix j .~ shift (c !! j) (-1)
+  | c2 !! j .&. 8 == 0 = return (c2, j)
+  | j2 >= start        = do{ liftIO $ putStrLn "               ***  Contract confirmed ***"; empty }
+  | otherwise          = do{ liftIO $ putStrLn "jkdhsfoiewrwelk"; ccrSubSub2 (cnt + 1) (c2 & ix j2 .~ shift (c2 !! j2) (-1)) j2 contract start } where
+      c2 = c & ix j .~ shift (c !! j) 1
       j2 = j - (length . takeWhile (/=0)) contract
+
+
+ccrSubSub3 :: Int -> [Int] -> Int
+ccrSubSub3 j contract
+  | j < 0              = 0
+  | contract !! j == 0 = j
+  | otherwise          = ccrSubSub3 (j - 1) contract
+
+
+ccrSubSub4 :: Int -> [Int] -> [Int] -> [Int] -> Int -> Int
+ccrSubSub4 u c dm sm i
+  | i > 4     = u
+  | otherwise = ccrSubSub4 u3 c dm sm (i + 1) where
+      u2 = if i <= dm !! 0 then u .|.             c !! (dm !! i)  else u
+      u3 = if i <= sm !! 0 then u .|. complement (c !! (sm !! i)) else u2
 
 
 inLive :: [Int] -> Int -> [Int] -> Int -> Bool
