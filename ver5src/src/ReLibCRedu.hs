89398de1ee99b53dd743a734f9d120d779c44318
righ1113 <34955220+righ1113@users.noreply.github.com>
2022-09-19 08:40:29 +0900

    22/09/19 Reduce : checkCReduce() の実装13

--- ver5src/src/ReLibCRedu.hs
+++ ver5src/src/ReLibCRedu.hs
@@ -14,20 +14,20 @@ import Data.Function                  ( fix )
 
 
 checkCReduce :: Int -> Int -> Int -> [Int] -> [[Int]] -> [[Int]] -> [Int] -> IO Bool
-checkCReduce ring bigno nLive live diffangle sameangle contract = do
+checkCReduce ring bigno nLive live diffAngle sameAngle contract = do
   _ <- if contract !! 0 /= 0         then return True else error "       ***  ERROR: NO CONTRACT PROPOSED  ***\n\n"
   _ <- if nLive == contract !! edges then return True else error " ***  ERROR: DISCREPANCY IN EXTERIOR SIZE  ***\n\n"
 
   let
-    start      = diffangle !! 0 !! 2
+    start      = diffAngle !! 0 !! 2
     c          = replicate edges 0
     forbidden  = replicate edges 0 -- called F in the notes
-    start2     = start - (length . takeWhile (/=0)) contract
+    start2     = ccrSubSub3 start contract
     c2         = c & ix start2 .~ 1
     j          = start2 - 1
-    j2         = j     - (length . takeWhile (/=0)) contract
-    dm         = diffangle !! j2
-    sm         = sameangle !! j2
+    j2         = ccrSubSub3 j contract
+    dm         = diffAngle !! j2
+    sm         = sameAngle !! j2
     c3         = c2 & ix j2 .~ 1
     u          = 4
     imax1      = dm !! 0
@@ -36,31 +36,33 @@ checkCReduce ring bigno nLive live diffangle sameangle contract = do
     u3         = foldl (\x y -> x .|. c3 !! y) u2 (take imax2 . tail $ sm)
     forbidden2 = forbidden & ix j2 .~ u3
 
+  putStrLn ("start2: " ++ show start2)
   putStrLn ("f c j: " ++ show forbidden2 ++ " " ++ show c3 ++ " " ++ show j2)
-  _ <- runMaybeT $ checkCReduceSub 0 forbidden2 c3 contract j2 start2 diffangle sameangle bigno ring live
+  _ <- runMaybeT $ checkCReduceSub 0 forbidden2 c3 contract j2 start2 diffAngle sameAngle bigno ring live
   return True
 
 
 checkCReduceSub :: Int -> [Int] -> [Int] -> [Int] -> Int -> Int -> [[Int]] -> [[Int]] -> Int -> Int -> [Int] -> MaybeT IO Bool
 checkCReduceSub 2097152 _ _ _ _ _ _ _ _ _ _      = error "checkCReduceSub : It was not good though it was repeated 2097152 times!"
-checkCReduceSub cnt forbidden c contract j start diffangle sameangle bigno ring live = do
+checkCReduceSub cnt forbidden c contract j start diffAngle sameAngle bigno ring live = do
   --liftIO $ putStrLn "checkCReduceSub start."
-  (c1, j1) <- ccrSubSub1 0 c  j  contract start forbidden
-  --let (c1, j1) = (c, j)
+  (c1, j1) <- ccrSubSub1 0 c j contract start forbidden
   --liftIO $ putStrLn "checkCReduceSub chk 1."
   _        <- if j1 == 1 && not (inLive c1 ring live bigno) then error "ERROR: INPUT CONTRACT IS INCORRECT  ***\n\n" else return True
   (c2, j2) <- ccrSubSub2 0 (c1 & ix j1 .~ shift (c1 !! j1) 1) j1 contract start
-  _        <- if j1 == 1 then checkCReduceSub (cnt + 1) forbidden c2 contract j2 start diffangle sameangle bigno ring live else return True
+  _        <- if j1 == 1 then checkCReduceSub (cnt + 1) forbidden c2 contract j2 start diffAngle sameAngle bigno ring live else return True
   _        <- if j2 <= 0 then error "checkCReduceSub : error!" else return True
+  --liftIO $ putStrLn "checkCReduceSub chk 2."
   let
-    j3         = ccrSubSub3 j1 contract
-    dm         = diffangle !! j3
-    sm         = sameangle !! j3
+    j3         = ccrSubSub3 (j1 - 1) contract
+    dm         = diffAngle !! j3
+    sm         = sameAngle !! j3
     c3         = c1 & ix j3 .~ 1
     u1         = 0
     u2         = ccrSubSub4 u1 c3 dm sm 1
     forbidden2 = forbidden & ix j3 .~ u2
-  checkCReduceSub (cnt + 1) forbidden2 c3 contract j3 start diffangle sameangle bigno ring live
+  --liftIO $ putStrLn "checkCReduceSub chk 3."
+  checkCReduceSub (cnt + 1) forbidden2 c3 contract j3 start diffAngle sameAngle bigno ring live
 
 
 ccrSubSub1 :: Int -> [Int] -> Int -> [Int] -> Int -> [Int] -> MaybeT IO ([Int], Int)
@@ -68,7 +70,7 @@ ccrSubSub1 100000 _ _ _ _ _ = error "ccrSubSub1 rec error!!"
 ccrSubSub1 cnt c j contract start forbidden
   | b         = return (c, j)
   | otherwise = do
-      --liftIO $ putStrLn ("f c j: " ++ show forbidden ++ " " ++ show c ++ " " ++ show j)
+      liftIO $ putStrLn ("c j: " ++ show c ++ " " ++ show j)
       (c3, j2) <- ccrSubSub2 0 c2 j contract start
       ccrSubSub1 (cnt + 1) c3 j2 contract start forbidden where
         b  = forbidden !! j .&. c !! j == 0
@@ -76,20 +78,27 @@ ccrSubSub1 cnt c j contract start forbidden
 
 
 ccrSubSub2 :: Int -> [Int] -> Int -> [Int] -> Int -> MaybeT IO ([Int], Int)
-ccrSubSub2 100000 _ _ _ _ = error "ccrSubSub2 rec error!!"
+ccrSubSub2 100 _ _ _ _ = error "ccrSubSub2 rec error!!"
 ccrSubSub2 cnt c j contract start
-  | c2 !! j .&. 8 == 0 = return (c2, j)
-  | j2 >= start        = do{ liftIO $ putStrLn "               ***  Contract confirmed ***"; empty }
-  | otherwise          = ccrSubSub2 (cnt + 1) (c2 & ix j2 .~ shift (c2 !! j2) (-1)) j2 contract start where
-      c2 = c & ix j .~ shift (c !! j) 1
-      j2 = j - (length . takeWhile (/=0)) contract
+  | c !! j .&. 8 == 0 = return (c, j)
+  | j2 >= start       = do{ liftIO $ putStrLn "               ***  Contract confirmed ***"; empty }
+  | otherwise         = do
+      liftIO $ putStrLn ("    c j: " ++ show c ++ " " ++ show j ++ " " ++ show j2)
+      ccrSubSub2 (cnt + 1) c2 j2 contract start where
+        j2 = ccrSubSub3_2 (j + 1) contract
+        c2 = c & ix j2 .~ shift (c !! j2) 1
 
 
 ccrSubSub3 :: Int -> [Int] -> Int
 ccrSubSub3 j contract
-  | j < 0              = 0
+  | j < 0              = 1
   | contract !! j == 0 = j
   | otherwise          = ccrSubSub3 (j - 1) contract
+ccrSubSub3_2 :: Int -> [Int] -> Int
+ccrSubSub3_2 j contract
+  | j > 100            = 100
+  | contract !! j == 0 = j
+  | otherwise          = ccrSubSub3_2 (j + 1) contract
 
 
 ccrSubSub4 :: Int -> [Int] -> [Int] -> [Int] -> Int -> Int
@@ -111,8 +120,8 @@ inLive col ring live bigno
                       _ | i > 2     -> (min1, max1)
                         | otherwise -> loop (min2, max2, i + 1) where
                             w    = weight !! i
-                            min2 = min w min0
-                            max2 = max w max0
+                            min2 = min w min1
+                            max2 = max w max1
       weight0      = replicate 5 0
       weight       = flip fix (weight0, 1) $ \loop (weight1, i) -> case () of
                       _ | i > ring  -> weight1
